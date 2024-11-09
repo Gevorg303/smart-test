88e890b499e560798eb3b71c2e02b6ae54225d7a
Gev <gevorg112003@mail.ru>
2024-11-09 14:46:57 +0300

    Изменил название сущности

--- backend/src/main/java/com/example/smart_test/domain/TaskOfIndicator.java
+++ backend/src/main/java/com/example/smart_test/domain/TaskOfIndicator.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "задание_по_индикатору")
+@Table(name = "задание_по_индикато")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
