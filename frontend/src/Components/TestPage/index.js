8df9a8b7136aeca1257b25b299f2b6a398b11c0d
Kotan Neon <kotan78744@gmail.com>
2024-12-21 17:30:14 +0300

    Добавлена пагинация для прохождения теста

--- frontend/src/Components/TestPage/index.js
+++ frontend/src/Components/TestPage/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import { React,useEffect, useState } from 'react';
 import Question from "../Question";
-import {Button} from 'react-bootstrap';
+import {Button, Pagination} from 'react-bootstrap';
 
 const TestPage = () => {
+
+    const [active, setActive] = useState(0);
+    let count = 4; // количество вопросов
+    let questions = []; //заполняем массив с вопросами из теста
+    for (let number = 0; number < count; number++) {
+        questions.push(
+            <Question id={number+1} description={number+1+" description"}/>
+        );
+    }
+    let paginationItems =[] //заполняем массив с кнопками для пагинации
+    for (let number = 0; number < count; number++) {
+        paginationItems.push(
+            <Pagination.Item key={number} active={number === active} onClick={() => setActive(number) }>
+                {number+1}
+            </Pagination.Item>
+        );
+    }
+    function TestEnd()
+    {
+        //
+
+        console.log("TestEnd");
+    }
+    useEffect( () => {
+
+    });
+
+
     return (
         <div>
             <h1>Контрольный тест</h1>
-            <Question id="1"/>
-            <Button>Предыдущий вопрос</Button>
-            <Button>Следующий вопрос</Button>
+            {questions[active]}
+            <Pagination>
+                <Pagination.Prev onClick={() => { if(active != 0)setActive(active - 1 )}}/>
+                {paginationItems}
+                <Pagination.Next onClick={() => { if(active < count-1)setActive(active + 1 )}}/>
+            </Pagination>
+            <Button onClick={() =>TestEnd()}>Завершить тест</Button>
         </div>
     );
 };
