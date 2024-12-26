32b18824eb6cf7813ca49f03274dd0e077dd94d8
Kotan Neon <kotan78744@gmail.com>
2024-12-27 02:16:29 +0300

    Исправлена ошибка

--- frontend/src/utils/router/privateRoute.js
+++ frontend/src/utils/router/privateRoute.js
@@ -13,7 +13,7 @@ export default function PrivateRoutes() {
     let  userid = getCookie("jwtToken")
     return (
         <>
-            {userid ? <Outlet  /> : <Navigate to="/" />};
+            {userid ? <Outlet  /> : <Navigate to="/" />}
         </>
 
     )
