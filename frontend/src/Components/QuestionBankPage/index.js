09b7ef21ad8e5d6381e71dde66bb83195d28bacd
ChumakDaniil404 <daniil404404@gmail.com>
2024-12-27 01:54:58 +0300

    Изменены стили дл кнопок

--- frontend/src/Components/QuestionBankPage/index.js
+++ frontend/src/Components/QuestionBankPage/index.js
@@ -1,59 +1,55 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import BankCard from "../BankCard";
-
+import "./styles.css";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 const QuestionBankPage = () => {
     const [isTests, setIsTests] = useState(true);
     const [tests, setTests] = useState([]);
     const [questions, setQuestions] = useState([]);
-  /*  const bank=[]
-    for (var i = 0; i < 5; i++) {
-        bank.push(i+100 + " test");
-    }*/
 
     useEffect(() => {
         async function fetchTests() {
             try {
-                    document.cookie = "sub=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
-                    document.cookie = "test=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
-                    const response1 = await fetch('http://localhost:8080/users/current', {
-                        credentials: "include",
-                    });
-                    if (!response1.ok) {
-                        throw new Error('Ошибка сети');
-                    }
-                    const user = await response1.json();
+                document.cookie = "sub=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+                document.cookie = "test=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+                const response1 = await fetch('http://localhost:8080/users/current', {
+                    credentials: "include",
+                });
+                if (!response1.ok) {
+                    throw new Error('Ошибка сети');
+                }
+                const user = await response1.json();
 
-                    const response2 = await fetch('http://localhost:8080/test/get-user-tests', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json;charset=UTF-8'
-                        },
-                        body: JSON.stringify(user)
-                    });
-                    if (!response2.ok) {
-                        throw new Error('Ошибка получения теста');
-                    }
-                    const tests = await response2.json();
-                    console.log(tests)
-                    setTests(tests)
+                const response2 = await fetch('http://localhost:8080/test/get-user-tests', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8'
+                    },
+                    body: JSON.stringify(user)
+                });
+                if (!response2.ok) {
+                    throw new Error('Ошибка получения теста');
+                }
+                const tests = await response2.json();
+                console.log(tests)
+                setTests(tests)
 
-                    const response3 = await fetch('http://localhost:8080/task/get-user-tasks', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json;charset=UTF-8'
-                        },
-                        body: JSON.stringify(user)
-                    });
-                    if (!response3.ok) {
-                        throw new Error('Ошибка получения теста');
-                    }
-                    const questions = await response3.json();
-                    console.log(questions)
-                    setQuestions(questions)
-
-
+                const response3 = await fetch('http://localhost:8080/task/get-user-tasks', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8'
+                    },
+                    body: JSON.stringify(user)
+                });
+                if (!response3.ok) {
+                    throw new Error('Ошибка получения теста');
+                }
+                const questions = await response3.json();
+                console.log(questions)
+                setQuestions(questions)
 
             } catch (error) {
                 console.error('Ошибка получения данных:', error);
@@ -65,10 +61,24 @@ const QuestionBankPage = () => {
 
     return (
         <div>
-            <Button onClick={()=>{setIsTests(true)}}>Тесты</Button>
-            <Button onClick={()=>{setIsTests(false)}}>Вопросы</Button>
-            <h1>{isTests?"Тесты":"Вопросы"}</h1>
-            {isTests?tests.map((item, index)=> <BankCard id={index} obj={item} isTest={isTests} />):questions.map((item, index)=> <BankCard id={index} obj={item} isTest={isTests} />)}
+            <Navbar/>
+            <br/><br/><br/>
+            <h1>{isTests ? "Тесты" : "Вопросы"}</h1>
+            <div className="page-container">
+
+                <div className="button-container">
+                    <Button className="button" onClick={() => {
+                        setIsTests(true)
+                    }}>Тесты</Button>
+                    <Button className="button" onClick={() => {
+                        setIsTests(false)
+                    }}>Вопросы</Button>
+                </div>
+                {isTests ? tests.map((item, index) => <BankCard key={index} id={index} obj={item}
+                                                                isTest={isTests}/>) : questions.map((item, index) =>
+                    <BankCard key={index} id={index} obj={item} isTest={isTests}/>)}
+            </div>
+            <Footer/>
         </div>
     );
 };
