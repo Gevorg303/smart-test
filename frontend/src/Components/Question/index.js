6b52d3edc89a79939ff6d51d164d75564b172f7d
AugustEidenberg <den.saraev14@gmail.com>
2024-12-26 22:16:41 +0300

    Сделаны стили для страницы просмотра результатов.

--- frontend/src/Components/Question/index.js
+++ frontend/src/Components/Question/index.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './styles.css'; // Импорт CSS файла для стилей компонента
 
-const Question = ({ id, qStatus, name, description, view, answers, setAnswers,setActive ,qsCount}) => {
+const Question = ({ id, qStatus, name, description, view, answers, setAnswers, setActive, qsCount }) => {
     const [currentAnswers, setCurrentAnswers] = useState(answers);
 
     const onClick = (id, answer) => {
         const array = [...answers];
         array[id] = answer;
         setAnswers(array);
-        setActive(prev=>qsCount===prev+1?prev:prev+1)
+        setActive(prev => qsCount === prev + 1 ? prev : prev + 1);
     };
 
     const handleInputChange = (id, answer) => {
@@ -19,20 +19,22 @@ const Question = ({ id, qStatus, name, description, view, answers, setAnswers,se
     };
 
     return (
-        <div className="question-container">
+        <div className={`question-container ${view ? (qStatus ? 'correct' : 'incorrect') : ''}`}>
             <div className="question-content">
-                <h2>{name} {qsCount} {view ? qStatus ? "(True)" : "(False)" : ""}</h2>
+                <h2>{name}</h2>
                 <h3>{description}</h3>
             </div>
             <div className="answer-section">
                 <Form>
                     {view ? (
-                        <Form.Control type="text"
-                                      placeholder="Ответ"
-                                      value={currentAnswers[id] || ""}
-                                      onChange={(e) => handleInputChange(id, e.target.value)}
-                                      required readOnly />
-
+                        <Form.Control
+                            type="text"
+                            placeholder="Ответ"
+                            value={currentAnswers[id] || ""}
+                            onChange={(e) => handleInputChange(id, e.target.value)}
+                            required
+                            readOnly
+                        />
                     ) : (
                         <Form.Control
                             type="text"
@@ -42,7 +44,7 @@ const Question = ({ id, qStatus, name, description, view, answers, setAnswers,se
                             required
                         />
                     )}
-                    <br/>
+                    <br />
                     {view ? (
                         <></>
                     ) : (
