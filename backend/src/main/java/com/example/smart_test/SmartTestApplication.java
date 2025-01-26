e04c9c304feadad73850180901a1f134814bc197
Gevorg <gevorg112003@mail.ru>
2025-01-26 20:37:23 +0300

    Переход на серверное БД https://ru.yougile.com/team/652e866a25b4/#SMART-5 + https://ru.yougile.com/team/652e866a25b4/#SMART-13

--- backend/src/main/java/com/example/smart_test/SmartTestApplication.java
+++ backend/src/main/java/com/example/smart_test/SmartTestApplication.java
@@ -1,5 +1,5 @@
 package com.example.smart_test;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SmartTestApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.datasource.url", dotenv.get("DATASOURCE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DATASOURCE_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DATASOURCE_PASSWORD"));
         SpringApplication.run(SmartTestApplication.class, args);
     }
 
