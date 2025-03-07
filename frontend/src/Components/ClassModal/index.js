2fae13b157fd530d5fe90ebffe2ebf5d12377125
ChumakDaniil404 <daniil404404@gmail.com>
2025-03-07 21:20:27 +0300

    Изменен компонент с модальным окном на странице Класс предмет. Добавлен компонент для модального окна со списком классов.

--- frontend/src/Components/ClassModal/index.js
+++ frontend/src/Components/ClassModal/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Button,Toast,ToastContainer } from 'react-bootstrap';
+import ClassSelector from "../ClassSelector";
 
-const ClassModal = () => {
+const ClassModal = ({targetSubject}) => {
+    const [classes, setClasses] = useState([]);
 
 
     return (
         <div>
             <h1>Выберите классы</h1>
             <Form  /*onSubmit={handleSubmit}*/>
-
+                <ClassSelector targetSubject = {targetSubject} indicators={classes} setIndicators={setClasses}/>
             </Form>
         </div>
     );
