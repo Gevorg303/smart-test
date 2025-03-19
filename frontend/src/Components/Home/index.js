620f67704e9fea2570bfdfec2a54ca957e860501
ChumakDaniil404 <daniil404404@gmail.com>
2025-03-19 18:03:09 +0300

    Реализовал макет для всех доступных страниц приложения

--- frontend/src/Components/Home/index.js
+++ frontend/src/Components/Home/index.js
@@ -1,15 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import SubjectCard from "../SubjectCard";
-import WelcomeComponent from "../WelcomeComponent";
 import "../Home/styles.css";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
-import { Button } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
 
 const HomePage = () => {
     const containerRef = useRef(null);
-    const [welcometext, setwelcometext] = useState("");
     const [subjects, setSubjects] = useState([]);
     const [topText, setTopText] = useOutletContext();
 
@@ -18,8 +13,6 @@ const HomePage = () => {
     useEffect(() => {
         async function fetchUser() {
             try {
-
-                //localStorage.setItem('info', "111");
                 document.cookie = "sub=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT";
                 document.cookie = "test=; path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT";
                 const response = await fetch('http://localhost:8080/users/current', {
@@ -31,7 +24,10 @@ const HomePage = () => {
                 const user = await response.json();
                 console.log(user);
                 setTopText("Здравствуйте, " + user.name + " (" + user.role.role + ")");
-                //setwelcometext("Здравствуйте, " + user.name + " (" + user.role.role + ")");
+
+                const container = document.querySelector('.all-container');
+                container.classList.add('page-style-1');
+
                 const response2 = await fetch('http://localhost:8080/subject/print-user-subject', {
                     method: 'POST',
                     headers: {
@@ -43,34 +39,26 @@ const HomePage = () => {
                     throw new Error('Ошибка вывода предметов учителя');
                 }
                 const subjectsJson = await response2.json();
-                const array = [];
-                subjectsJson.forEach(subject => {
-                    array.push(
-                        <SubjectCard key={subject.id} id={subject.id} name={subject.subjectName} description={subject.description} />
-                    );
-                });
+                const array = subjectsJson.map(subject => (
+                    <SubjectCard key={subject.id} id={subject.id} name={subject.subjectName} description={subject.description} />
+                ));
                 setSubjects(array);
             } catch (error) {
                 console.error('Ошибка получения данных:', error);
             }
         }
+
         fetchUser();
     }, [setTopText]);
 
     return (
-        <>
-            <div className="home-page">
-                {/*<div className="welcome">
-                    {/*<WelcomeComponent text={welcometext} />}
-                </div>*/}
-                <div className="container-wrapper">
-                    <div className="container-home" id="subjects-container" ref={containerRef} data-count={subjects.length}>
-                        {subjects}
-                    </div>
+        <div className="home-page">
+            <div className="container-wrapper">
+                <div className="container-home" id="subjects-container" ref={containerRef} data-count={subjects.length}>
+                    {subjects}
                 </div>
             </div>
-
-        </>
+        </div>
     );
 };
 
--- frontend/src/Components/Layout/index.js
+++ frontend/src/Components/Layout/index.js
@@ -1,6 +1,6 @@
 // Layout.js
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Handbook from "../Handbook";
 import './styles.css';
@@ -9,20 +9,54 @@ import Footer from "../Footer";
 const Layout = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [topText, setTopText] = useState();
+    const location = useLocation();
+
+    useEffect(() => {
+        const container = document.querySelector('.all-container');
+        const container_top = document.querySelector('.top-text');
+
+        if (container) {
+            if (location.pathname.includes('register') || location.pathname.includes('profile')) {
+                container.classList.add('page-style-1');
+            } else {
+                container.classList.add('page-style-2');
+            }
+        }
+
+        if (container_top) {
+            if (location.pathname.includes('register') || location.pathname.includes('profile')) {
+                container_top.classList.add('page-style-top-1');
+            } else {
+                container_top.classList.add('page-style-top-2');
+            }
+        }
+
+        // Функция очистки
+        return () => {
+            if (container) {
+                container.classList.remove('page-style-1', 'page-style-2');
+            }
+            if (container_top) {
+                container_top.classList.remove('page-style-top-1', 'page-style-top-2');
+            }
+        };
+        }, [location.pathname]);
 
     return (
         <>
-            <Navbar setShowHandbook={setIsModalOpen}/>
+            <Navbar setShowHandbook={setIsModalOpen} />
             <div className="all-content">
                 <div className="top-text">{topText}</div>
                 <div className="all-container">
-                    <Outlet context={[topText, setTopText]}/>
+                    <Outlet context={[topText, setTopText]} />
                 </div>
             </div>
-            <Handbook isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
-            <Footer/>
+            <Handbook isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+            <footer className="footer">
+                <Footer />
+            </footer>
         </>
     );
 };
 
-export default Layout;
+            export default Layout;
