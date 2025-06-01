bbd3b843646c45e998aef838302cdbcd33af2390
Gevorg <gevorg112003@mail.ru>
2025-06-01 10:16:07 +0300

    Доработка редактирования пользователя

---
+++ backend/src/main/java/com/example/smart_test/request/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.example.smart_test.request;
+
+import com.example.smart_test.dto.RoleDto;
+import com.example.smart_test.dto.StudentClassDto;
+import com.example.smart_test.dto.UserDto;
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private UserDto user; // пользователь
+    private RoleDto role; // роль
+    private StudentClassDto studentClass; // класс
+
+    public UserUpdateRequest(UserDto user, RoleDto role, StudentClassDto studentClass) {
+        this.user = user;
+        this.role = role;
+        this.studentClass = studentClass;
+    }
+}
