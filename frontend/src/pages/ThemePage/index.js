d0e4611b2336f138d3b99e4713d0ca2ac1559d7c
ChumakDaniil404 <daniil404404@gmail.com>
2025-06-11 21:57:42 +0300

    1) Добавил заглушки на страницы

--- frontend/src/pages/ThemePage/index.js
+++ frontend/src/pages/ThemePage/index.js
@@ -94,7 +94,7 @@ const ThemePage = () => {
                 </div>*/}
                 <div className="theme-container">
                     <h3>Доступные Вам темы:</h3>
-                        {themes}
+                    {themes.length > 0 ? themes : <p>Нет доступных тем.</p>}
                 </div>
                 </div>
             {/*<Footer/>*/}
